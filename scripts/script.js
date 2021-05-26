9bcd7dd38bc3f0ac892d1f9cc889c4204afd30b0
Rayan Wilbert <rayan.rmw@gmail.com>
2021-05-26 17:00:03 -0300

    Open/close modal function added

---
+++ scripts/script.js
@@ -0,0 +1,32 @@
+/* =============   MODAL  =============  */
+const modal = {
+    open() {
+        document.querySelector(".modal-overlay").classList.add("active");
+    },
+    close() {
+        document.querySelector(".modal-overlay").classList.remove("active");
+    }
+}
+
+// function openCloseForm() {   // ====== ALTERNATE FUNCTION=========
+//     document.querySelector(".modal-overlay").classList.toggle("active");
+// }
+
+/* =============   STORAGE  =============  */
+
+
+/* =============   TRANSACTIONS  =============  */
+
+/* =============   DOM  =============  */
+
+/* =============   UTILS  =============  */
+
+/* =============   FORM  =============  */
+
+/* =============   APP  =============  */
+
+
+
+
+
+
