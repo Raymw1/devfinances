b85a0e726131ccc1bef8cade3600a2bd82201765
Rayan Wilbert <rayan.rmw@gmail.com>
2021-05-26 17:33:41 -0300

    Add transaction function added, without form

--- scripts/script.js
+++ scripts/script.js
@@ -13,17 +13,120 @@ const modal = {
 // }
 
 /* =============   STORAGE  =============  */
-
+const transactions = [
+    {
+        description: "Computador",
+        amount: -500000,
+        date: "25/05/2021"
+    },
+    {
+        description: "Vendas",
+        amount: 500000,
+        date: "26/05/2021"
+    }
+]
 
 /* =============   TRANSACTIONS  =============  */
+const Transaction = {
+    all: transactions,
+    add() {
+
+    },
+    remove() {
+
+    },
+    incomes() {
+
+    },
+    expenses() {
+
+    },
+    total() {
+
+    }
+}
+
 
 /* =============   DOM  =============  */
+const DOM = {
+    transactionsContainer: document.querySelector("#data-table tbody"),
+    addTransaction(transaction, index) {
+        const tr = document.createElement("tr");
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction, index);
+        tr.dataset.index = index;
+        DOM.transactionsContainer.appendChild(tr);
+    },
+    innerHTMLTransaction(transaction, index) {
+        const CSSClass = transaction.amount > 0 ? "income" : "expense";
+        const amount = utils.formatCurrency(transaction.amount);
+        const html = `
+        <td class="description">${transaction.description}</td>
+        <td class="${CSSClass}">${amount}</td>
+        <td class="date">${transaction.date }</td>
+        <td><img onclick="Transaction.remove(${index})" src="assets/minus.svg  " alt="Remover transação"></td>`;
+        return html;
+    },
+    updateBalance() {
+
+    },
+    clearTransactions() {
+
+    }
+}
+
 
 /* =============   UTILS  =============  */
+const utils = {
+    formatAmout(value) {
+
+    },
+    formatDate(date) {
+
+    },
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : "";
+        value = String(value).replace(/\D/g, "")    // Remove anything that is not number
+        value = Number(value) / 100;
+        value = value.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        })
+        return signal + value;
+    }
+}
+
 
 /* =============   FORM  =============  */
+const form = {
+    getValues() {
+
+    },
+    validateFields() {
+
+    },
+    formatValues() {
+
+    },
+    clearFields() {
+
+    },
+    submit() {
+
+    }
+}
+
 
 /* =============   APP  =============  */
+const app = {
+    init() {
+        transactions.forEach(DOM.addTransaction);
+    },
+    reload() {
+
+    }
+}
+
+app.init()
 
 
 
