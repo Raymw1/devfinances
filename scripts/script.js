862207a1b6d072f04c10fcee35b817f3e9f034a4
Rayan Wilbert <rayan.rmw@gmail.com>
2021-05-27 16:25:25 -0300

    Add transaction in form function added

--- scripts/script.js
+++ scripts/script.js
@@ -29,20 +29,29 @@ const transactions = [
 /* =============   TRANSACTIONS  =============  */
 const Transaction = {
     all: transactions,
-    add() {
-
+    add(transaction) {
+        Transaction.all.push(transaction);
+        app.reload();
     },
     remove() {
 
     },
     incomes() {
-
+        let income = 0;
+        Transaction.all.forEach(function(transaction) {
+            income += transaction.amount > 0 ? transaction.amount : 0;
+        })
+        return income;
     },
     expenses() {
-
+        let expense = 0;
+        Transaction.all.forEach(function(transaction) {
+            expense += transaction.amount < 0 ? transaction.amount : 0;
+        })
+        return expense;
     },
     total() {
-
+        return Transaction.incomes() + Transaction.expenses();
     }
 }
 
@@ -67,10 +76,12 @@ const DOM = {
         return html;
     },
     updateBalance() {
-
+        document.getElementById("incomesDisplay").innerHTML = utils.formatCurrency(Transaction.incomes());
+        document.getElementById("expensesDisplay").innerHTML = utils.formatCurrency(Transaction.expenses());
+        document.getElementById("totalDisplay").innerHTML = utils.formatCurrency(Transaction.total());
     },
     clearTransactions() {
-
+        DOM.transactionsContainer.innerHTML = ""
     }
 }
 
@@ -78,10 +89,13 @@ const DOM = {
 /* =============   UTILS  =============  */
 const utils = {
     formatAmout(value) {
-
+        value = Number(value) * 100;
+        value = Math.round(value);
+        return value;
     },
     formatDate(date) {
-
+        const splittedDate = date.split("-")
+        return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`;
     },
     formatCurrency(value) {
         const signal = Number(value) < 0 ? "-" : "";
@@ -98,20 +112,45 @@ const utils = {
 
 /* =============   FORM  =============  */
 const form = {
+    description: document.querySelector("form input#description"),
+    amount: document.querySelector("form input#amount"),
+    date: document.querySelector("form input#date"),
     getValues() {
-
+        return {
+            description: form.description.value,
+            amount: form.amount.value,
+            date: form.date.value
+        }
     },
     validateFields() {
-
+        const { description, amount, date } = form.getValues();
+        if (description.trim() === "" || amount.trim() === "" || date.trim() === "") {
+            throw new Error("Por favor, preencha todos os campos!");
+        }
     },
     formatValues() {
-
+        let { description, amount, date } = form.getValues();
+        amount = utils.formatAmout(amount);
+        date = utils.formatDate(date);
+        console.log(description, amount, date)
+        return  { description, amount, date };
     },
     clearFields() {
-
+        form.description.value = "";
+        form.amount.value = "";
+        form.date.value = "";
     },
-    submit() {
-
+    submit(event) {
+        event.preventDefault();
+        try {
+            form.validateFields();
+            const transaction = form.formatValues();
+            Transaction.add(transaction);
+            form.clearFields();
+            modal.close();
+        } catch(error) {
+            alert(error.message);
+        }
     }
 }
 
@@ -120,9 +159,11 @@ const form = {
 const app = {
     init() {
         transactions.forEach(DOM.addTransaction);
+        DOM.updateBalance();
     },
     reload() {
-
+        DOM.clearTransactions();
+        app.init()
     }
 }
 
